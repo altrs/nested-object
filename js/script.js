7b0011b6f58bd87f5ab0bad69ae4b8ec28c5453d
Robin Altman <robinhaltman@gmail.com>
2023-10-10 16:46:57 -0400

    debugged intital

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const internet = {
 
+  internet_layers: ['Physical Layer', 'Data Link Layer', 'Network Layer', 'Transport Layer', 'Session Layer', 'Presentation Layer', 'Application Layer'],
+  internet_exchange_points: 600, //approximate
+  switches: undefined, //a really big number
+
   server: {
     ip_address: '',
     name: '',
@@ -14,7 +18,7 @@ const internet = {
       ethernet: [],
     },
     firewall: true,
-    wireless_access_point: true;
+    wireless_access_point: true,
   },
 
   computer: {
@@ -25,7 +29,7 @@ const internet = {
   packet: {
   	hop_count: 0,
   	hop_limit: 128,
-  	traced_route: [];
+  	traced_route: [],
 	ip_header: '',
 	udp_header: {
 		intended_application: undefined,
@@ -52,7 +56,7 @@ const internet = {
   },
 
   dns_registry: {
-  	dns_root_servers: 13;
+  	dns_root_servers: 13,
   	top_level_domains: ['.com', '.net', '.org', '.int', '.edu', '.gov', '.mil'], 
   	second_level_domains: [],
   	sub_domains: [],
@@ -66,29 +70,32 @@ const internet = {
   		https: undefined, //true or false
   		http: undefined, //true or false
   	},
+  	webcrawler: {
+  		known_sites: [],
+  		index:[],
+  	},
   	webcrawler: function(){
   		console.log("Webcrawler is crawling");
+  		known_sites.forEach((element, index) => {
+  			console.log('Checking content of: ' + element);
+  			console.log('Adding to index');
+  		});
   	},
   },
 
-  internet_layers: ['Physical Layer', 'Data Link Layer', 'Network Layer', 'Transport Layer', 'Session Layer', 'Presentation Layer', 'Application Layer'],
-  internet_exchange_points: 600, //approximate
-  switches: undefined, //a really big number
+  check_DNS_registry: function (newDNS) {
+  	console.log('Consulting DNS Registry');
+  	second_level_domains.forEach((element, index) => {
+	  if(newDNS == element){console.log('domain name taken.')}
+	  else{
+	  	second_level_domains.append(newDNS);
+	  	console.log('Domain registered');
+	  }
+	});
+  },
 
-  check_DNS_registry: function () {
-
+  connect_to_website: function (siteName) {
+  	console.log("Connecting to " + siteName);
   }
-
-  congestion_control: function () {
-
-  }
-
-  packet_switch: function () {
-
-  }
-
-  connect_to_website: function () {
-
-  }
-
+  
 };
