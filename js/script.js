1b02c57e8b783daf3f887e0507d719da9f5dfbd8
Robin Altman <robinhaltman@gmail.com>
2023-10-11 00:04:33 -0400

    fin for wed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
 const internet = {
 
 	internet_layers: ['Physical Layer', 'Data Link Layer', 'Network Layer', 'Transport Layer', 'Session Layer', 'Presentation Layer', 'Application Layer'],
-	internet_exchange_points: 600, //approximate
-	switches: undefined, //a really big number
-	routers: [],
+	internet_exchange_points: 600, //approximate apparently
+	switches: undefined, //a really big number that I'm unsure about
+	servers: [], //to be filled dynamically
+	routers:[],
+	computers: [],
+	packets: [],
+	users_worldwide: 5300000000,
+	amount_of_websites: 1130000000,
+	internet_birthday: 'January 1, 1983',
+	major_applications: ['Communication', 'Web Browsing', 'Social Media', 'E-commerce, Entertainment and Streaming', 'Cloud Computing Services'],
 
-	server: {
-		ip_address: '',
-		name: '',
-		webpages: {
-		  files: [],
-		},
+	addServer: function(ip_address, name, files) {
+		let server = {
+			ip_address: ip_address,
+			name: name,
+			webpages: {
+			  files: files || [],
+			},
+		}
+		internet.servers.push(server);
+		internet.servers.forEach((element, index) => {
+			console.log("Servers: " + internet.servers[index].name);
+		});
 	},
 
 	addRouter: function(name, wanPorts, ethernetPorts, firewall, wirelessAccessPoint) {
 		let router = {
 			name: name,
 			ports: {
-				wan: [],
-				ethernet: [],
+				wan: wanPorts || [],
+				ethernet: ethernetPorts || [],
 			},
 			firewall: true,
 			wireless_access_point: true,
@@ -29,38 +42,56 @@ const internet = {
 		});
 	},
 
-	computer: {
-		ip_address: undefined,
-		mac_address: undefined,
+	addComputer: function(ip, mac){
+		let computer = {
+			ip_address: ip,
+			mac_address: mac,
+		};
+		internet.computers.push(computer);
+		internet.computers.forEach((element, index) => {
+			console.log("Computers: " + internet.computers[index].ip_address);
+		});
 	},
 
-	packet: {
-		hop_count: 0,
-		hop_limit: 128,
-		traced_route: [],
-		ip_header: '',
-		udp_header: {
-			intended_application: undefined,
-			port_number: undefined,
-			checksum: undefined,
-			checksum: function() {
-				console.log("checking data sum");
+	addPacketUDP: function(hop_count, tracedRoute, ip_header, application, port, checksum, data){
+		let packet = {
+			hop_count: hop_count,
+			hop_limit: 128,
+			traced_route: tracedRoute, //array
+			ip_header: ip_header,
+			udp_header: {
+				intended_application: application,
+				port_number: port,
+				checksum: checksum,
+				checksum: function() {
+					console.log("checking data sum");
+				},
 			},
-		},
-		tcp_header: {
-			intended_application: undefined,
-			port_number: undefined,
-			checksum: undefined,
-			sequence_number: undefined,
-			ack: undefined,
-			checksum: function() {
-				console.log("checking data sum");
+			data_payload: data,
+		}
+	},
+
+	addPacketTCP: function(hop_count, tracedRoute, ip_header, application, port, checksum, sequence_num, data){
+		let packet = {
+			hop_count: hop_count,
+			hop_limit: 128,
+			traced_route: tracedRoute,
+			ip_header: ip_header,
+			tcp_header: {
+				intended_application: application,
+				port_number: port,
+				checksum: checksum,
+				sequence_number: sequence_num,
+				ack: false,
+				checksum: function(packet) {
+					console.log("checking data sum");
+				},
+				acknowledge: function(packet) {
+					this.ack = true;
+				},
 			},
-			acknowledge: function() {
-				this.ack = true;
-			},
-		},
-		data_payload: {},
+			data_payload: data,
+		};
 	},
 
 	dns_registry: {
@@ -68,6 +99,9 @@ const internet = {
 		top_level_domains: ['.com', '.net', '.org', '.int', '.edu', '.gov', '.mil'], 
 		second_level_domains: [],
 		sub_domains: [],
+		addDomain: function(domain, level){
+			console.log("Adding " + domain + " to " + level); //add actual functionality?
+		},
 	},
 
 	world_wide_web: {
@@ -109,4 +143,6 @@ const internet = {
 };
 
 internet.addRouter('My Router', ['WAN3'], ['Eth3', 'Eth4'], true, true);
+internet.addServer('102.453.12', 'myweb', ['index.html', 'style.css']);
+internet.addComputer('102.453.12', '13.356.543');
 
